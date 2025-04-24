bc5d00ee9b7e34364f14726d0592cc8b84f678e8
tashfiq9 <tashfiq9@gmail.com>
2025-04-25 04:30:24 +0600

    Update ShopConntext.jsx

--- frontend/src/Context/ShopConntext.jsx
+++ frontend/src/Context/ShopConntext.jsx
@@ -15,12 +15,12 @@ const ShopConntextProvider=(props)=> {
 
     const [all_product, setAll_Product] = useState([]);
     useEffect(()=>{
-     fetch('http://localhost:4000/allproducts')
+     fetch('${process.env.REACT_APP_API_URL}/allproducts')
      .then((response)=>response.json())
      .then((data)=>setAll_Product(data))
 
      if(localStorage.getItem('auth-token')){
-        fetch('http://localhost:4000/getcart',{
+        fetch('${process.env.REACT_APP_API_URL}/getcart',{
             method:'POST',
             headers:{
                 Accept: 'application/form-data',
@@ -40,7 +40,7 @@ const ShopConntextProvider=(props)=> {
     const addToCart=(itemId) =>{
          setCartItems((prev)=> ({...prev, [itemId]:prev[itemId]+1}))
          if(localStorage.getItem('auth-token')){
-            fetch('http://localhost:4000/addtocart', {
+            fetch('${process.env.REACT_APP_API_URL}/addtocart', {
                 method: 'POST',
                 headers:{
                     Accept: 'application/form-data',
@@ -58,7 +58,7 @@ const ShopConntextProvider=(props)=> {
         
         if(localStorage.getItem('auth-token')){
             
-            fetch('http://localhost:4000/removefromcart', {
+            fetch('${process.env.REACT_APP_API_URL}/removefromcart', {
                 method: 'POST',
                 headers:{
                     Accept: 'application/form-data',
